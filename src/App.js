3549773f992fbae6809d9fffd7372796d6eefdbd
Muhammed Bilal Omeiza <104626610+Bilcruz1@users.noreply.github.com>
2025-09-30 10:29:50 +0100

    landing page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/header";
-import Home from "./components/Home/home";
-import Footer from "./components/Footer/footer";
-import About from "./components/About/about.jsx";
-import Program from "./components/Program/program";
-import ProgramDetail from "./components/Program/ProgramDetail";
-import News from "./components/News/news";
-import Contact from "./components/Contact/contact";
-import Donate from "./components/Donate/donate";
-import ScrollToTop from "./components/ScrollToTop.js";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/header';
+import Home from './components/Home/home.jsx';
+import Footer from './components/Footer/footer';
+import About from './components/About/about.jsx';
+import Program from './components/Program/program';
+import ProgramDetail from './components/Program/ProgramDetail';
+import News from './components/News/news';
+import Contact from './components/Contact/contact';
+import Donate from './components/Donate/donate';
+import ScrollToTop from './components/ScrollToTop.js';
 
 function App() {
-  return (
-    <Router>
-      <ScrollToTop />
-      <Header />
+	return (
+		<Router>
+			<ScrollToTop />
+			<Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/program" element={<Program />} />
-        <Route path="/program/:id" element={<ProgramDetail />} />{" "}
-        <Route path="/news" element={<News />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/donate" element={<Donate />} />
-      </Routes>
+			<Routes>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
+				<Route
+					path="/about"
+					element={<About />}
+				/>
+				<Route
+					path="/program"
+					element={<Program />}
+				/>
+				<Route
+					path="/program/:id"
+					element={<ProgramDetail />}
+				/>{' '}
+				<Route
+					path="/news"
+					element={<News />}
+				/>
+				<Route
+					path="/contact"
+					element={<Contact />}
+				/>
+				<Route
+					path="/donate"
+					element={<Donate />}
+				/>
+			</Routes>
 
-      <Footer />
-    </Router>
-  );
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
